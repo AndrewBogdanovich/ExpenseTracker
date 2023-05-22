0d6e8c599e3bb4b5a75ef463b710aeee4c5c2ce4
AndrewBohdanovich <andrew.bogdanovich@cogniteq.com>
2023-05-22 18:11:39 +0300

    add compose preview for different devices

---
+++ app/src/main/java/com/example/expensetracker/ComposePreview.kt
@@ -0,0 +1,24 @@
+package com.example.expensetracker
+
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(
+    name = "Phone",
+    group = "Devices",
+    showSystemUi = true,
+    device = Devices.PIXEL
+)
+annotation class PhonePreview
+
+@Preview(
+    name = "Tablet",
+    group = "Devices",
+    showSystemUi = true,
+    device = Devices.TABLET
+)
+annotation class TabletPreview
+
+@PhonePreview
+@TabletPreview
+annotation class DevicesPreview
--- app/src/main/java/com/example/expensetracker/MainActivity.kt
+++ app/src/main/java/com/example/expensetracker/MainActivity.kt
@@ -15,11 +15,10 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.expensetracker.ui.theme.ExpenseTrackerTheme
@@ -91,8 +90,7 @@ fun SigInView() {
         }
     }
 }
-
-@Preview(showBackground = true, showSystemUi = true)
+@DevicesPreview
 @Composable
 fun SignInPreview() {
     SigInView()
